941a4159f0cf8398ce179de87f2e66316fddfdb7
Morgan Winkler <winkler.morgan1553@gmail.com>
2024-01-05 14:35:30 -0500

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "*", "localhost:5173", "https://happiest-hour.onrender.com"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
